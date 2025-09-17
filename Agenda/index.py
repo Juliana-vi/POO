ae7ccb5d9140433b891d20698925fb5e5b0bdf17
Juliana-vi <j.vianna@escolar.ifrn.edu.br>
2025-09-17 04:30:09 +0000

    Implantação do streamlit - agenda

---
+++ Agenda/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+
+RetanguloUI.main()
+
+
+#st.header("Olá mundo")
+#if st.button("Clique aqui"):
+#    st.write("Minha primeira página em Python")
