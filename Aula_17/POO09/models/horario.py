8fff2d89f3c8bdf4ab0078e4d4e2599d25b00457
Juliana-vi <j.vianna@escolar.ifrn.edu.br>
2025-09-25 04:09:21 +0000

    Inserção de cadastro de horários

--- Aula_17/POO09/models/horario.py
+++ Aula_17/POO09/models/horario.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import json
 
 class Horario:
   def __init__(self, id, data):
@@ -31,8 +32,65 @@ class Horario:
 
   @staticmethod
   def from_json(dic):
-    horario = Horario(dic["id"], datetime.strptime(dic["data"], "%d/%m/%Y%H:%M"))
+    horario = Horario(dic["id"], datetime.strptime(dic["data"], "%d/%m/%Y %H:%M"))
     horario.set_confirmado(dic["confirmado"])
     horario.set_id_cliente(dic["id_cliente"])
     horario.set_id_servico(dic["id_servico"])
     return horario
+  
+class HorarioDAO:
+  __objetos = []
+
+  @classmethod
+  def inserir(cls, obj):
+    cls.abrir()
+    id = 0
+    for aux in cls.__objetos:
+      if aux.get_id() > id: id = aux.get_id()
+    obj.set_id(id+1)
+    cls.__objetos.append(obj)
+    cls.salvar()
+
+  @classmethod
+  def listar(cls):
+    cls.abrir()
+    return cls.__objetos
+
+  @classmethod
+  def listar_id(cls, id):
+    cls.abrir()
+    for obj in cls.__objetos:
+      if obj.get_id() == id: return obj
+    return None
+
+  @classmethod
+  def atualizar(cls, obj):
+    aux = cls.listar_id(obj.get_id())
+    if aux != None:
+      cls.__objetos.remove(aux)
+      cls.__objetos.append(obj)
+      cls.salvar()
+
+  @classmethod
+  def excluir(cls, obj):
+    aux = cls.listar_id(obj.get_id())
+    if aux != None:
+      cls.__objetos.remove(aux)
+      cls.salvar()
+
+  @classmethod
+  def abrir(cls):
+    cls.__objetos = []
+    try:
+      with open("horarios.json", mode="r") as arquivo:
+        list_dic= json.load(arquivo)
+        for dic in list_dic: 
+          obj = Horario.from_json(dic)
+          cls.__objetos.append(obj)
+    except FileNotFoundError:
+      pass
+
+  @classmethod
+  def salvar(cls):
+    with open("horarios.json", mode ="w") as arquivo:
+      json.dump(cls.__objetos, arquivo, default= Horario.to_json)
--- Aula_17/POO09/views.py
+++ Aula_17/POO09/views.py
@@ -1,27 +1,71 @@
 from models.cliente import Cliente, ClienteDAO
 from models.servico import Servico, ServicoDAO
+from models.horario import Horario, HorarioDAO
 
 class View:
   def cliente_listar():
     return ClienteDAO.listar()
+  
   def cliente_inserir(nome, email, fone):
     cliente = Cliente(0, nome, email, fone)
     ClienteDAO.inserir(cliente)
+
   def cliente_atualizar(id, nome, email, fone):
     cliente = Cliente(id, nome, email, fone)
     ClienteDAO.atualizar(cliente)
+
   def cliente_excluir(id):
     cliente = Cliente(id, "", "", "")
     ClienteDAO.excluir(cliente)
 
+  @staticmethod
+  def cliente_listar_id(id):
+    clientes = View.cliente_listar()
+    for cliente in clientes:
+      if cliente.get_id() == id:
+        return cliente
+    return None
+
   def servico_listar():
     return ServicoDAO.listar()
+  
   def servico_inserir(descricao, valor):
     servico = Servico(0, descricao, valor)
     ServicoDAO.inserir(servico)
+
   def servico_atualizar(id, descricao, valor):
     servico = Servico(id, descricao, valor)
     ServicoDAO.atualizar(servico)
+
   def servico_excluir(id):
     servico = Servico(id, "", 0.0)
     ServicoDAO.excluir(servico)
+
+  @staticmethod
+  def servico_listar_id(id):
+    servicos = View.servico_listar()
+    for servico in servicos:
+      if servico.get_id() == id:
+        return servico
+    return None
+
+  def horario_inserir(data, confirmado, id_cliente, id_servico):
+    c = Horario(0, data)
+    c.set_confirmado(confirmado)
+    c.set_id_cliente(id_cliente)
+    c.set_id_servico(id_servico)
+    HorarioDAO.inserir(c)
+
+  def horario_listar():
+    return HorarioDAO.listar()
+  
+  def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+    c= Horario(id, data)
+    c.set_confirmado(confirmado)
+    c.set_id_cliente(id_cliente)
+    c.set_id_servico(id_servico)
+    HorarioDAO.atualizar(c)
+
+  def horario_excluir(id):
+    c = Horario(id, None)
+    HorarioDAO.excluir(c)
